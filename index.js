f8e7810fadba571e95454959b60f192f4805d2d4
Rey <rsantelli@luc.edu>
2024-01-31 22:39:44 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+// Function to return the first two drivers
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // Function to return the last two drivers
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // Array containing both of the above functions
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Function that returns a fare multiplier function
+  const createFareMultiplier = function(multiplier) {
+    return function(value) {
+      return multiplier * value;
+    };
+  };
+  
+  // Doubler and tripler functions using the multiplier function
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+  
+  // Function to select different driver functions
+  const selectDifferentDrivers = function(arrayOfDrivers, selectorFunction) {
+    return selectorFunction(arrayOfDrivers);
+  };
